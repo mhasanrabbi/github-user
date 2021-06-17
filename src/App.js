653e5162d025594c8be8fa4dcef1b16ba0fa7fbd
mahmudhasanrabbi <mdmahmudhasanrabbi@gmail.com>
2021-06-17 18:05:02 +0600

    react router error

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      <Switch>
       <Route exact path='/'>
         <Dashboard></Dashboard>
       </Route>
       <Route path='/login'>
         <Login />
       </Route>
+      <Route path="*">
+        <Error></Error>
+      </Route>
+      </Switch>
     </Router>
   );
 }
